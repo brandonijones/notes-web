0c2b78d0dccdae11edc7204b803448d6f0c0e158
brandonijones <65355965+brandonijones@users.noreply.github.com>
2023-09-17 14:21:18 -0400

    NOTE-2: Demo basic dynamic routing

---
+++ src/app/hello/[name]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Params = {
+  params: {
+    name: string
+  }
+}
+
+export default function HelloName({ params: { name }}: Params) {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">Hello, { name }!</div>
+  )
+}
